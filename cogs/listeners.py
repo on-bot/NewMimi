cee9695d284ebf656327fc412eb493a2bd109ad8
ASHH <30630722+onii-bot@users.noreply.github.com>
2023-01-02 21:06:37 +0545

    Update listeners.py

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands
 import datetime
+import json
+
+conf = open("./cogs/config.json")
+config = json.load(conf)
+
+# Ethos
+ethos = config['guilds']['ethos']
 
 
 class Listeners(commands.Cog):
@@ -28,46 +35,20 @@ class Listeners(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.guild.id == 1039314094081183824:  # ethos
-            knight = discord.utils.get(message.guild.roles, id=1045279846953132072)
-            bishop = discord.utils.get(message.guild.roles, id=1045280247903424582)
-            rook = discord.utils.get(message.guild.roles, id=1045279621958090872)
-            queen = discord.utils.get(message.guild.roles, id=1045274556379701259)
-            king = discord.utils.get(message.guild.roles, id=1045274429141299250)
-            server_booster = discord.utils.get(message.guild.roles, id=1040681728232136795)
-            role_list = [knight, bishop, rook, queen, king, server_booster]
-
-            if check_list(role_list, message.author.roles):
-                valid = True
-            else:
+        if message.guild.id == ethos['guild_id']:
+            knight = discord.utils.get(message.guild.roles, id=ethos['role_ids']['knight_id'])
+            if message.author.top_role < knight:
                 valid = False
-
+            else:
+                valid = True
         else:
             valid = True
+
         if valid:
-            if message.guild.id == 988374126681030656 and message.channel.id == 988374129226965012:
-                if message.content == "/resend-roles":
-                    file = discord.File("crew3_select.png", filename="crew3_select.png")
-                    await message.reply("Make sure to select Crew3 bot while typing the command", file=file)
+            if message.content.startswith('.test'):
+                await message.reply("Test Successfull")
 
-            if message.guild.id == 988374126681030656:
-                if 'no role' in message.content or "haven't got role" in message.content or "haven't got my role" in message.content:
-                    await message.reply(
-                        "go to <#988374129226965012> and type /resend-roles make sure to select crew3 bot to get your roles")
-
-            if message.guild.id == 1039314094081183824:
-                if message.content.lower() == "meow" or message.content.lower()[0:9] == "mimi meow":
-                    await message.reply("nyaaa :cat: ")
-                elif message.content.lower()[0:9] == "mimi come":
-                    await message.reply("NO")
-                elif message.content.lower() == "i love u" or message.content.lower()[0:13] == "mimi i love u":
-                    await message.reply("I love u too :kissing_smiling_eyes: ")
-                elif message.content.lower() == "mimi ttyl" or message.content.lower() == "mimi talk to you later":
-                    await message.reply("noooooooo")
-                elif message.content[-1] == "?" and "mimi" in message.content.lower():
-                    await message.reply(random.choice(["yes", "no", "hmmmm", "meowww", "maybe", "idk", "perhaps"]))
-
-        await client.process_commands(message)
+        await self.client.process_commands(message)
 
 
 async def setup(client):
