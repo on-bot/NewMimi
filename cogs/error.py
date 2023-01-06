65c8baeb0291776fd2c5c0a6f45529e76f31f412
ASHH <30630722+onii-bot@users.noreply.github.com>
2023-01-06 18:22:51 +0545

    Update error.py

--- cogs/error.py
+++ cogs/error.py
@@ -6,40 +6,11 @@ from discord.ext import commands
 
 class CommandErrorHandler(commands.Cog):
 
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, client):
+        self.client = client
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        """The event triggered when an error is raised while invoking a command.
-        Parameters
-        ------------
-        ctx: commands.Context
-            The context used for command invocation.
-        error: commands.CommandError
-            The Exception raised.
-        """
-
-        # This prevents any commands with local handlers being handled here in on_command_error.
-        if hasattr(ctx.command, 'on_error'):
-            return
-
-        # This prevents any cogs with an overwritten cog_command_error being handled here.
-        cog = ctx.cog
-        if cog:
-            if cog._get_overridden_method(cog.cog_command_error) is not None:
-                return
-
-        ignored = (commands.CommandNotFound, )
-
-        # Allows us to check for original exceptions raised and sent to CommandInvokeError.
-        # If nothing is found. We keep the exception passed to on_command_error.
-        error = getattr(error, 'original', error)
-
-        # Anything in ignored will return and prevent anything happening.
-        if isinstance(error, ignored):
-            return
-
         if isinstance(error, commands.DisabledCommand):
             await ctx.send(f'{ctx.command} has been disabled.')
 
@@ -51,8 +22,10 @@ class CommandErrorHandler(commands.Cog):
 
         # For this error example we check to see where it came from...
         elif isinstance(error, commands.BadArgument):
-            if ctx.command.qualified_name == 'tag list':  # Check if the command being invoked is 'tag list'
-                await ctx.send('I could not find that member. Please try again.')
+            await ctx.send('I could not find that member. Please try again. :(')
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please mention a user :(")
 
         else:
             # All other Errors not returned come here. And we can just print the default TraceBack.
