98e0f6f3c00e20ca11b4a53b562178045ce617c8
onii <fabianmann9@gmail.com>
2023-03-11 14:31:36 +0545

    kic command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -106,6 +106,54 @@ class Moderation(commands.Cog):
                 await ctx.send(f"Couldn't find {len(left_over)} users")
 
     @commands.command()
+    async def kic(self, ctx):
+        """ Remove Roles to Given list of Usernames (Works Usernames, File) """
+        if ctx.author.guild_permissions.manage_roles:
+            left_over = []
+            successful = []
+            await ctx.send("Reply with discord usernames (follow the format)")
+
+            def check(m):
+                return m.author.id == ctx.author.id
+
+            message = await self.client.wait_for('message', check=check, timeout=120)
+            if not message.author.bot:
+                await ctx.send("on it :cat:")
+                if message.attachments:
+                    attachment = message.attachments[0]
+                    with attachment.save() as file:
+                        username_list = file
+                else:
+                    msg = message.content
+                    username_list = msg.split('\n')
+                for username in username_list:
+                    username = username.rstrip()
+                    try:
+                        namez, id = username.split('#')
+                        user = discord.utils.get(ctx.guild.members, name=namez, discriminator=id)
+                    except:
+                        user = None
+                    if user == None:
+                        left_over.append(username)
+                    else:
+                        await user.kick()
+                        successful.append(username)
+                wled = "**Successful**"
+                for i in successful:
+                    wled = wled + "\n" + i
+                nwled = "**Not Found**"
+                for i in left_over:
+                    nwled = nwled + "\n" + i
+
+                if 15 < len(wled) < 1900:
+                    await message.channel.send(wled)
+                if 14 < len(nwled) < 1900:
+                    await message.channel.send(nwled)
+
+                await ctx.send(f"Successfully done for {len(successful)} users")
+                await ctx.send(f"Couldn't find {len(left_over)} users")
+
+    @commands.command()
     async def assign(self, ctx, role: discord.Role):
         """ On the replied message it assigns roles to mentioned users """
         if ctx.message.reference:
