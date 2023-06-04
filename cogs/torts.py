4318964318fac15927795b3ebe05d4ef633a7627
ASHH <30630722+onii-bot@users.noreply.github.com>
2023-06-04 16:59:05 +0545

    Update torts.py

--- cogs/torts.py
+++ cogs/torts.py
@@ -15,10 +15,13 @@ class Torts(commands.Cog):
     
     @commands.Cog.listener()
     async def on_guild_channel_create(self, channel):
+        print(torts["guild_id"])
         if channel.guild.id == torts["guild_id"]:
             await asyncio.sleep(2)
+            print("test")
             first_message = [message async for message in channel.history(limit=1, oldest_first=True)][0]
             if first_message.author.id == 557628352828014614:
+                print("should be no error")
                 nameO = first_message.mentions[0].name
                 await channel.edit(name=nameO)
 
