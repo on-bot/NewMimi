019e6af3f83ba4f1e324680131c7bc51c7d6d775
onii <fabianmann9@gmail.com>
2023-06-04 16:17:41 +0545

    added auto name change of channel

---
+++ cogs/torts.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+import json
+import asyncio
+
+conf = open("./cogs/config.json")
+config = json.load(conf)
+
+# Torts
+torts = config['guilds']['test']
+
+
+class Torts(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.Cog.listener()
+    async def on_guild_channel_create(self, channel):
+        if channel.guild.id == torts["guild_id"]:
+            await asyncio.sleep(2)
+            first_message = [message async for message in channel.history(limit=1, oldest_first=True)][0]
+            if first_message.author.id == 557628352828014614:
+                nameO = first_message.mentions[0].name
+                await channel.edit(name=nameO)
+
+
+async def setup(client):
+    await client.add_cog(Torts(client))
